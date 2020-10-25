6b96edbfed05b6e200b2190f777e9f9bf241f1c0
Pierre Salumu <jrsalumu@gmail.com>
2020-10-25 12:25:39 -0700

    wk4As

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        $(".carousel").carousel("pause");
+            if($("#carouselButton").children("i").hasClass("fa-pause")) {
+                $(".carousel").carousel("pause");
+                $("#carouselButton").children("i").removeClass("fa-pause");
+                $("#carouselButton").children("i").addClass("fa-play");
+            } else {
+                $(".carousel").carousel("cycle");
+                $("#carouselButton").children("i").removeClass("fa-play");
+                $("#carouselButton").children("i").addClass("fa-pause");
+            }
+    });
+});    
+
+$(function() {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("show");
+    });
+});
+
+/*
+$(function() {
+    $('a[href$="#reserveButton"]').click(function () {
+        $("#reserveModal").modal("show");
+    });
+});
+
+$(function() {
+    $('a[href$="#loginButton"]').click(function() {
+        $("#loginModal").modal("show");
+    });
+});
+*/
